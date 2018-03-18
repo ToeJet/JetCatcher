44953abdf499b79370980a0745d2d1a9d172c8b2
James Toebes <james@hp15tr100.home.toebesacademy.com>
2018-03-17 21:09:07 -0400

    Adding historical info

--- getfeeds.files.php
+++ getfeeds.files.php
@@ -2,7 +2,7 @@
 <body>
 <form action="" method="post">
 
-<select size=15 style="width:100%" onchange='ShowPlayer(this.value)'>
+<select id="PlayList" size=15 style="width:100%" onchange='ShowPlayer(this.value)'>
 <?php
 $filesDir = scandir("files");
 $numfiles = count($filesDir);
@@ -13,41 +13,83 @@ for($i=0; $i<$numfiles; $i++)
     $fname =  $filesDir[$i];
     if ((substr($fname,0,1)==".")==false) {
         if (is_dir($fname)==false) {
-            echo "<option value='" . $fname ."'>" . $fname . "</option>";
+            echo "<option id=". $i . " value='" . $fname ."'>" . $fname . "</option>";
         }
     }
 }
 ?>
 </select>
 <script>
+function PlayEnded()
+{
+    $fdir = "<?php echo getcwd(); ?>/files/";
+    $fname = document.getElementById("PlayList").value;
+
+    if (document.getElementById("autodelete").checked == true) {
+        unlink($fdir . document.getElementById("PlayList").value);
+    }
+
+    if (document.getElementById("autoadvance").checked == true) {
+        SelectPlayer(1);
+    }
+}
+function SelectPlayer(nav)
+{
+    document.getElementById("curplayer").pause();
+    $newid = document.getElementById("PlayList").options.selectedIndex + nav;
+    $newvalue = document.getElementById("PlayList").options[$newid].value;
+    document.getElementById("PlayList").value = $newvalue;
+    ShowPlayer($newvalue);
+}
 function ConfirmDelete(fname)
 {
     document.getElementById("cDel").innerHTML = "Delete " + fname + " <a href='getfeeds.fileremove.php?remfile=" + fname + "'>Confirm</a>";
 }
 function ShowPlayer(fname)
 {
+    $pmode = "audio";
+    $ptype = "audio/mpeg";
     switch (fname.substring(fname.lastIndexOf("."))) {
     case ".ogg":
-        document.getElementById("fPlayer").innerHTML = "<audio controls> <source src='files/" + fname + "' type='audio/ogg'></audio>";
+        $pmode = "audio";
+        $ptype = "audio/ogg";
         break;
     case ".mp3":
-        document.getElementById("fPlayer").innerHTML = "<audio controls> <source src='files/" + fname + "' type='audio/mpeg'></audio>";
+        $pmode = "audio";
+        $ptype = "audio/mpeg";
         break;
     case ".m4a":
-        document.getElementById("fPlayer").innerHTML = "<audio controls> <source src='files/" + fname + "' type='audio/mpeg'></audio>";
+        $pmode = "audio";
+        $ptype = "audio/mpeg";
         break;
     case ".mp4":
-        document.getElementById("fPlayer").innerHTML = "<video controls> <source src='files/" + fname + "' type='video/mp4'></video>";
+        $pmode = "video";
+        $ptype = "video/mpeg";
         break;
     case ".wav":
-        document.getElementById("fPlayer").innerHTML = "<audio controls> <source src='files/" + fname + "' type='audio/mpeg'></video>";
+        $pmode = "audio";
+        $ptype = "audio/mpeg";
         break;
     }
-    document.getElementById("fPlayer").innerHTML = document.getElementById("fPlayer").innerHTML + "<br />"
-                                                 + "<button type='button' onclick='ConfirmDelete(\"" + fname + "\")'>X</button>"
-                                                 + "<label style='background-color:cyan'><a target='player' href='files/" + fname + "'>" + fname + "</a></label>";
+    document.getElementById("fPlayer").innerHTML = "<" + $pmode + " id=curplayer controls autoplay onended='PlayEnded()' > <source src='files/" + fname + "' type='" + $ptype + "'></" + $pmode + ">"
+												 + "<br />"
+                                                 + "<button type='button' onclick='SelectPlayer(-1)'>&lt</button>"
+                                                 + "<button type='button' onclick='SelectPlayer(1)'>&gt</button>"
+                                                 + "<label style='background-color:cyan'><a target='player' href='files/" + fname + "'>" + fname + "</a></label>"
+                                                 + "<button type='button' onclick='ConfirmDelete(\"" + fname + "\")'>X</button>" ;
+;
+
+    //echo "hi \n";	
+
+    if (document.getElementById("autoplay").checked == true) {
+        document.getElementById("curplayer").autoplay="autoplay";
+        document.getElementById("curplayer").play();
+    }
 }
 </script>
+<input id='autoplay' type='checkbox'><label for="autoplay">Auto Play</label>
+<input id='autoadvance' type='checkbox'><label for="autoadvance">Auto Advance</label>
+<!-- <input id='autodelete' type='checkbox'><label for="autodelete">Auto Delete</label> -->
 <p id="fPlayer"></p><br />
 <p id="cDel"></p></form>
 </body>
